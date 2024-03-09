0e7dd947b66b622a767ba431918551297dfdf8c0
ibrahim.kedir <ibrahim.kedir@outlook.com>
2024-03-09 16:32:12 -0500

    update to file structure
    - created / deleted folders and new files to begin coding

---
+++ my-app/src/Components/Code.tsx
@@ -0,0 +1,17 @@
+
+function Code() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          
+        </header>
+      </div>
+    );
+  }
+  
+  export default Code;
+  
---
+++ my-app/src/Components/SingleMessage.tsx
@@ -0,0 +1,18 @@
+
+
+function SingleMessage() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          
+        </header>
+      </div>
+    );
+  }
+  
+  export default SingleMessage;
+  
